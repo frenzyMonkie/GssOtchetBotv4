e6f2bba237e15968d4545af720f6786acb1f8759
Vasiliy <66134410+frenzyMonkie@users.noreply.github.com>
2024-05-14 12:34:21 +0300

    Update swipe.js

--- swipe.js
+++ swipe.js
@@ -12,7 +12,7 @@ const _C = document.querySelector('.slider'),
 				'ease-in-out': function(k) {
 					return .5*(Math.sin((k - .5)*Math.PI) + 1)
 				}, */
-				'bounce-out': function(k, a = 8.75, b = 1.5) {
+				'bounce-out': function(k, a = 8.75, b = 9.5) {
 					return 1 - Math.pow(1 - k, a)*Math.abs(Math.cos(Math.pow(k, b)*(n + .5)*Math.PI))
 				}
 			};
