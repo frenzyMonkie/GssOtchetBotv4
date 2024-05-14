976b86f410157f038b756fc2a25c217b83699fda
Vasiliy <66134410+frenzyMonkie@users.noreply.github.com>
2024-05-14 22:09:18 +0300

    1

---
+++ editdata.js
@@ -0,0 +1,8 @@
+
+applyBtn = document.getElementById("applyBtn")
+applyBtn.onmousedown = function() {
+    applyBtn.classList.toggle('applyBtn_clicked');
+};
+applyBtn.onmouseup = function() {
+    applyBtn.classList.toggle('applyBtn_clicked');
+};
