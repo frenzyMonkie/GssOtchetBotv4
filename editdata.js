865b9952c3a154255f404d74e352df2fb8c5ee98
Vasiliy <66134410+frenzyMonkie@users.noreply.github.com>
2024-05-14 22:23:30 +0300

    Revert "1"
    This reverts commit 976b86f410157f038b756fc2a25c217b83699fda.

--- editdata.js
+++
@@ -1,8 +0,0 @@
-
-applyBtn = document.getElementById("applyBtn")
-applyBtn.onmousedown = function() {
-    applyBtn.classList.toggle('applyBtn_clicked');
-};
-applyBtn.onmouseup = function() {
-    applyBtn.classList.toggle('applyBtn_clicked');
-};
